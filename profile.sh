894ad7504d67c6bc16fbdb0a8d95f063a88a26c5
P.J. Finlay <pjappdevelopment@gmail.com>
2021-05-01 07:18:47 -0500

    Configured git to use vim

--- profile.sh
+++ profile.sh
@@ -1,5 +1,6 @@
 # Configure Git
 git config --global user.name "P.J. Finlay"
 git config --global user.email "pjappdevelopment@gmail.com"
+git config --global core.editor "vim"
 
 alias venv="virtualenv env && source env/bin/activate"
