e5b876c76baae5d963be0a47e59786fd87e37731
P.J. Finlay <pjappdevelopment@gmail.com>
2022-07-01 07:54:44 -0500

    Add black package install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y vim git python3 python3-pip python3-virtualenv python-is-python3 zip
+sudo apt-get install -y vim git python3 python3-pip python3-virtualenv python-is-python3 zip black
