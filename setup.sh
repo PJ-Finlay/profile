45760459b00e93bb1954e7d1205333c71ae09a26
P.J. Finlay <pjappdevelopment@gmail.com>
2022-07-28 19:00:52 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y vim git python3 python3-pip python3-virtualenv python-is-python3 zip black
+sudo apt-get install -y vim git python3 python3-pip python3-virtualenv python-is-python3 zip black bmon htop
